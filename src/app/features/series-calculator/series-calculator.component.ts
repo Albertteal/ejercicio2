3e679b91fad7c3cb7d441232c7080db15e6e57ca
AlbertPumping <rocktteal@gmail.com>
2025-01-23 21:24:31 -0600

    Se agrega lógica básica para series-calculator

--- src/app/features/series-calculator/series-calculator.component.ts
+++ src/app/features/series-calculator/series-calculator.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-series-calculator',
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./series-calculator.component.css']
 })
 export class SeriesCalculatorComponent {
+  @Input() n: number | null = null; // Recibe el valor de "n" desde InputFormComponent
+  result: number | null = null;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['n'] && this.n !== null) {
+      this.calculateSeries(this.n);
+    }
+  }
+
+  calculateSeries(n: number): void {
+    this.result = 0;
+    for (let i = 1; i <= n; i++) {
+      this.result += i;
+    }
+  }
+
+
 
 }
